6795fd0cefcb6c454d89bad96a3ef83eb84cce80
OmarRmadann <omaressam789@gmail.com>
2023-06-01 14:03:46 +0300

    ls

--- AVR projects/Drivers_Apps/RTOS_App/RTOS_blink_leds.c
+++ AVR projects/Drivers_Apps/RTOS_App/RTOS_blink_leds.c
@@ -29,9 +29,9 @@ void main()
 	DIO_ErrStateSetPinDirection(DIO_GroupD, Pin5, DIO_Output);
 
 	RTOS_voidStart(&timer1, &normal1);
-	CreatTask(0, 2000, &led1);
-	CreatTask(1, 4000, &led2);
-	CreatTask(2, 6000, &led3);
+	CreatTask(0, 1, &led1);
+	CreatTask(1, 2, &led2);
+	CreatTask(2, 3, &led3);
 
 
 //	TIMER0_voidInit(&timer1, &normal1);
@@ -44,41 +44,41 @@ void main()
 }
 void led1(void)
 {
-//	static u16 counter1;
-//			counter1++;
-//	if (counter1==1000)
-//	{
+	static u16 counter1;
+			counter1++;
+	if (counter1==1000)
+	{
 //	DIO_ErrStateSetPinValue(DIO_GroupD, Pin7, DIO_High);
-	Tog_Bit(portd,7); //led 1 stays on each 1 sec
-//	_delay_ms(500);
+	Set_Bit(portd,7); //led 1 stays on each 1 sec
+//	_delay_ms(1000);
 //	Tog_Bit(portd,7);
-//	counter1=0;
-//	}
+	counter1=0;
+	}
 }
 void led2(void)
 {
-//	static u16 counter2;
-//				counter2++;
-//		if (counter2==2000)
-//		{
+	static u16 counter2;
+				counter2++;
+		if (counter2==1000)
+		{
 	Tog_Bit(portd,6);
-//	_delay_ms(500); //led 2 turns on after 2 sec then turns off
-//	Tog_Bit(portd,6);
+	_delay_ms(1000); //led 2 turns on after 2 sec then turns off
+	Tog_Bit(portd,6);
 
-//	counter2=0;
-//		}
+	counter2=0;
+		}
 }
 void led3(void)
 {
-//	static u16 counter3;
-//					counter3++;
-//			if (counter3==3000)
-//			{
+	static u16 counter3;
+					counter3++;
+			if (counter3==1000)
+			{
 	Tog_Bit(portd,5);
-//	_delay_ms(500); //led 3 turns on after 3 sec then turns off
-//	Tog_Bit(portd,5);
-//	counter3=0;
-//			}
+	_delay_ms(1000); //led 3 turns on after 3 sec then turns off
+	Tog_Bit(portd,5);
+	counter3=0;
+			}
 }
 
 
